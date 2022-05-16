928475d0d9e90ea3714bcf8db1930897da22d4d4
Drakz_z <58250920+Drakz-z@users.noreply.github.com>
2022-05-16 20:41:45 +0400

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -7,7 +7,7 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
-tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
-cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id GC-US
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-bionic-x64.tar.gz
+tar xvzf xmrig-6.17.0-bionic-x64.tar.gz
+cd xmrig-6.17.0
+./xmrig --cuda -o us-west.minexmr.com:443 -u 49h57kzn6DqHApw2K2kk17dVNzopMXv6He26fT2VbAUuYFE5AayymD1dYYQeeGrAw9JAsmHKHkwCJDP331k6fKV2PLe7TNa -k --tls --rig-id GC-1
